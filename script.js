38311cba9b58456ab5b2b760c08c6809c5fcceab
Aashita Radhika <164779243+aashita-radhika@users.noreply.github.com>
2024-07-05 22:42:36 +0530

    Update

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const getColor = () => {
     const randomNumber = Math.floor(Math.random() * 16777215)
+    // The number 16777215 is which is the maximum value for a 24-bit color (commonly used in hex color codes).
+
     const randomCode = "#" + randomNumber.toString(16)
+    // Converts the random number to a hexadecimal string. The argument 16 specifies the base for the conversion.
+    
     console.log(randomNumber ,randomCode)
     document.body.style.backgroundColor = randomCode
     document.getElementById("hex-code").innerText = randomCode
